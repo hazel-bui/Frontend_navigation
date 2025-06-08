4ad5bc3b8a2020e5f48563d4795fba3973ded4cb
hazel-bui <thibth22411c@st.uel.edu.vn>
2025-06-08 20:13:38 +0700

    Remove left sidebar

--- app/src/main/java/com/example/navbotdialog/MainActivity.java
+++ app/src/main/java/com/example/navbotdialog/MainActivity.java
@@ -8,28 +8,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.Fragment;
 
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.navigation.NavigationView;
 
-import android.app.Dialog;
 import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.view.Gravity;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
 import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.fragment.app.Fragment;
 
 public class MainActivity extends AppCompatActivity {
     FloatingActionButton fab;
@@ -40,37 +28,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        NavigationView navigationView = findViewById(R.id.layoutMainNavigationView);
-        // Lấy View header của NavigationView
-        View headerView = navigationView.getHeaderView(0);
-
-        // Truy cập 2 TextView trong nav_header.xml
-        TextView txtNavigationName = headerView.findViewById(R.id.txtNavigationName);
-        TextView txtNavigationEmail = headerView.findViewById(R.id.txtNavigationEmail);
-
-        // Truyền dữ liệu từ Java
-        txtNavigationName.setText("Rebound Piercing Vietnam");
-        txtNavigationEmail.setText("contact@rebound.vn");
 
         bottomNavigationView = findViewById(R.id.layoutMainBottomNavigationView);
         drawerLayout = findViewById(R.id.layoutMainDrawer);
-        fab = findViewById(R.id.imgMainButtonScan);
         Toolbar toolbar = findViewById(R.id.layoutMainToolbar);
         setSupportActionBar(toolbar);
 
-        // Bỏ dòng này nếu không cần icon mặc định (vì ta dùng nút custom)
         getSupportActionBar().setDisplayHomeAsUpEnabled(false);
         getSupportActionBar().setHomeButtonEnabled(false);
 
-        // Lấy nút menu từ toolbar
         ImageButton btnMenu = toolbar.findViewById(R.id.imgMainButtonMenu);
         btnMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Mở Drawer
-                drawerLayout.openDrawer(GravityCompat.START);
+                //Không mở menu kế bên
+                /*drawerLayout.openDrawer(GravityCompat.START);*/
             }
         });
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getWindow().setStatusBarColor(Color.TRANSPARENT);
             getWindow().getDecorView().setSystemUiVisibility(
@@ -78,17 +53,12 @@ public class MainActivity extends AppCompatActivity {
             );
         }
 
-
         if (savedInstanceState == null){
-            getSupportFragmentManager().beginTransaction().replace(R.id.layoutMainFrame, new ShopActivity()).commit();
-            navigationView.setCheckedItem(R.id.txtNavigationHome);
+            replaceFragment(new ShopActivity());
         }
-
-        replaceFragment(new ShopActivity());
 
         bottomNavigationView.setBackground(null);
         bottomNavigationView.setOnItemSelectedListener(item -> {
-
             int id = item.getItemId();
 
             if (id == R.id.txtNavigationShop) {
@@ -103,78 +73,12 @@ public class MainActivity extends AppCompatActivity {
 
             return true;
         });
-
-        /*fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showBottomDialog();
-            }
-        });*/
-
     }
 
-    // Outside onCreate
     private void replaceFragment(Fragment fragment) {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.layoutMainFrame, fragment);
-        fragmentTransaction.commit();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.layoutMainFrame, fragment)
+                .commit();
     }
-
-    private void showBottomDialog() {
-
-        final Dialog dialog = new Dialog(this);
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        dialog.setContentView(R.layout.bottomsheetlayout);
-
-        LinearLayout videoLayout = dialog.findViewById(R.id.layoutVideo);
-        LinearLayout shortsLayout = dialog.findViewById(R.id.layoutShorts);
-        LinearLayout liveLayout = dialog.findViewById(R.id.layoutLive);
-        ImageView cancelButton = dialog.findViewById(R.id.cancelButton);
-
-        videoLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                dialog.dismiss();
-                Toast.makeText(MainActivity.this,"Upload a Video is clicked",Toast.LENGTH_SHORT).show();
-
-            }
-        });
-
-        shortsLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                dialog.dismiss();
-                Toast.makeText(MainActivity.this,"Create a short is Clicked",Toast.LENGTH_SHORT).show();
-
-            }
-        });
-
-        liveLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                dialog.dismiss();
-                Toast.makeText(MainActivity.this,"Go live is Clicked",Toast.LENGTH_SHORT).show();
-
-            }
-        });
-
-        cancelButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dialog.dismiss();
-            }
-        });
-
-        dialog.show();
-        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
-        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-        dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
-        dialog.getWindow().setGravity(Gravity.BOTTOM);
-
-    }
-
 }
